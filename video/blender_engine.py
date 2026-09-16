99cb5a8449408edb154c29ca618c2b7b8c4031de
worlofgrim-ship-it <worlofgrim@gmail.com>
2026-09-16 10:10:27 -0600

    Refactor Blender scene generation functions

--- video/blender_engine.py
+++ video/blender_engine.py
@@ -1,5 +1,4 @@
 import bpy
-import math
 
 
 def clear_scene():
@@ -17,13 +16,12 @@ def create_black_hole():
     bpy.ops.mesh.primitive_uv_sphere_add(
         segments=128,
         ring_count=64,
-        location=(0,0,0)
+        location=(0, 0, 0)
     )
 
     black_hole = bpy.context.object
 
     black_hole.name = "Black Hole"
-
 
     black_hole.scale = (
         3,
@@ -43,7 +41,6 @@ def create_black_hole():
         1
     )
 
-
     black_hole.data.materials.append(
         material
     )
@@ -55,34 +52,24 @@ def create_earth():
     bpy.ops.mesh.primitive_uv_sphere_add(
         segments=64,
         ring_count=32,
-        location=(8,0,0)
+        location=(8, 0, 0)
     )
-
 
     earth = bpy.context.object
 
     earth.name = "Earth"
 
 
-    earth.scale = (
-        1,
-        1,
-        1
-    )
-
-
     material = bpy.data.materials.new(
         "Earth Material"
     )
 
-
     material.diffuse_color = (
-        0.1,
+        0.05,
         0.3,
         1,
         1
     )
-
 
     earth.data.materials.append(
         material
@@ -93,14 +80,12 @@ def create_earth():
 def create_camera():
 
     bpy.ops.object.camera_add(
-        location=(15,-15,8)
+        location=(15, -15, 8)
     )
-
 
     camera = bpy.context.object
 
     camera.name = "Cinematic Camera"
-
 
     bpy.context.scene.camera = camera
 
@@ -110,47 +95,46 @@ def create_light():
 
     bpy.ops.object.light_add(
         type="POINT",
-        location=(5,-5,10)
+        location=(5, -5, 10)
     )
-
 
     light = bpy.context.object
 
     light.name = "Space Light"
 
-
     light.data.energy = 2000
 
 
 
-def animate():
+def animate_camera():
+
+    camera = bpy.context.scene.camera
+
 
     bpy.context.scene.frame_start = 1
 
     bpy.context.scene.frame_end = 240
 
 
-    bpy.context.scene.camera.location = (
+    camera.location = (
         15,
         -15,
         8
     )
 
-
-    bpy.context.scene.camera.keyframe_insert(
+    camera.keyframe_insert(
         "location",
         frame=1
     )
 
 
-    bpy.context.scene.camera.location = (
+    camera.location = (
         5,
         -5,
         3
     )
 
-
-    bpy.context.scene.camera.keyframe_insert(
+    camera.keyframe_insert(
         "location",
         frame=240
     )
@@ -158,6 +142,10 @@ def animate():
 
 
 def generate_scene():
+
+    print(
+        "Generating Blender scene..."
+    )
 
     clear_scene()
 
@@ -169,13 +157,15 @@ def generate_scene():
 
     create_light()
 
-    animate()
+    animate_camera()
 
 
     print(
-        "Blender scene generated"
+        "Blender scene generated successfully"
     )
 
 
 
-generate_scene()
+if __name__ == "__main__":
+
+    generate_scene()
