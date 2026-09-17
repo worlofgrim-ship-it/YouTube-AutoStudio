a2d0ad97157c6b12cef3c453d06075abf8523467
worlofgrim-ship-it <worlofgrim@gmail.com>
2026-09-16 21:59:54 -0600

    Implement video upload function in uploader.py

---
+++ youtube/uploader.py
@@ -0,0 +1,16 @@
+def upload_video(
+    video,
+    title,
+    description
+):
+
+    print("Uploading:")
+    print(video)
+
+    print(title)
+
+    print(description)
+
+    print(
+        "YouTube upload ready"
+    )
