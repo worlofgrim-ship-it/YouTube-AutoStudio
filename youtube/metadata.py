348f474b3fc6a9f262cc634f80521ab540f7967d
worlofgrim-ship-it <worlofgrim@gmail.com>
2026-09-16 22:32:59 -0600

    Add create_metadata function for video metadata

---
+++ youtube/metadata.py
@@ -0,0 +1,41 @@
+def create_metadata(topic):
+
+
+    return {
+
+
+        "title":
+
+        topic,
+
+
+        "description":
+
+        f"""
+Welcome to Nundas!
+
+Today we explore:
+
+{topic}
+
+Subscribe for more science and what-if videos.
+
+#shorts #science #space
+""",
+
+
+        "tags":
+
+        [
+
+            "science",
+
+            "space",
+
+            "what if",
+
+            "shorts"
+
+        ]
+
+    }
